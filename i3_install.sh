53389aec709175e605fa9be0fc7aca0f141ead38
Hi-T <mccaffrey.brendan@gmail.com>
2017-04-01 16:21:18 +0100

    Add files via upload

---
+++ i3_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+usr/lib/apt/apt-helper download-file http://dl.bintray.com/i3/i3-autobuild-ubuntu/pool/main/i/i3-autobuild-keyring/i3-autobuild-keyring_2016.10.01_all.deb keyring.deb SHA256:460e8c7f67a6ae7c3996cc8a5915548fe2fee9637b1653353ec62b954978d844
+apt install ./keyring.deb
+echo 'deb http://dl.bintray.com/i3/i3-autobuild-ubuntu xenial main' > /etc/apt/sources.list.d/i3-autobuild.list
+apt update
+apt install i3
