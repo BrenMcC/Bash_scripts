b65a22a7539dc38fddf0f6b191f43ac8bce6647b
Hi-T <mccaffrey.brendan@gmail.com>
2017-04-01 16:25:17 +0100

    Update i3_install.sh

--- i3_install.sh
+++ i3_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Script to install i3 window manager in ubuntu 16.04
+
 
 usr/lib/apt/apt-helper download-file http://dl.bintray.com/i3/i3-autobuild-ubuntu/pool/main/i/i3-autobuild-keyring/i3-autobuild-keyring_2016.10.01_all.deb keyring.deb SHA256:460e8c7f67a6ae7c3996cc8a5915548fe2fee9637b1653353ec62b954978d844
 apt install ./keyring.deb
